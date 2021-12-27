840af7258e35c93b84decc7b26987abfe6ad7870
Ohdaejin <xla3095@gmail.com>
2021-12-27 11:40:51 +0900

    product_list 템플릿에서 layout 상속, ddt 활성화

--- products/views.py
+++ products/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from products.models import Product
 
 
 def product_list(request: HttpRequest):
+    product_list = Product.objects.order_by('-id')
+
+    return render(request, "products/product_list.html", {
+        "product_list": product_list
+    })
     return HttpResponse("안녕")
